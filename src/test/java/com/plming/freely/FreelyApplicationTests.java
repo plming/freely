9672c3ac6234b03f255db8e9086dcca6af59775e
Hyeonseok Kim <kim010322@gmail.com>
2025-06-14 22:26:02 +0900

    first commit

---
+++ src/test/java/com/plming/freely/FreelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plming.freely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
